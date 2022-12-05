cf2942432b004022e5a28c2b00ae6a500af84587
Steven Gutierrez <stevogbmv@gmail.com>
2022-12-05 11:24:59 -0800

    commit tree fb417e85db2f139c4d59464fb2b7432e0317af30

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array, matchName){
+    return array.filter(function (name){
+        return name.toLowerCase() === matchName.toLowerCase()
+    });
+}
+function fuzzyMatch(array, matchName){
+    return array.filter(
+        function (name) {
+            return name.toLowerCase().indexOf(matchName.toLowerCase()) === 0
+        }
+    )
+}
+function matchName(array, matchName){
+    return array.filter(function (driver){
+        return driver.name === matchName
+    })
+}
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// console.log (findMatching(drivers,'Bobby'))
